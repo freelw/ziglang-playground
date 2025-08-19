90c701e8e8712bd005925620e4ce7697e167aefe
freelw <826231693@qq.com>
2025-08-19 15:08:32 +0800

    base64

---
+++ base64/src/main.zig
@@ -0,0 +1,27 @@
+const Base64 = struct {
+    _table: *const [64]u8,
+
+    pub fn init() Base64 {
+        const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const lower = "abcdefghijklmnopqrstuvwxyz";
+        const numbers_symb = "0123456789+/";
+        return Base64{
+            ._table = upper ++ lower ++ numbers_symb,
+        };
+    }
+
+    pub fn _char_at(self: Base64, index: usize) u8 {
+        return self._table[index];
+    }
+};
+
+pub fn main() !void {
+    const base64 = Base64.init();
+    const stdout_file = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout_file);
+    const stdout = bw.writer();
+    try stdout.print("Character at index 28: {c}\n", .{base64._char_at(28)});
+    try bw.flush(); // Don't forget to flush!
+}
+
+const std = @import("std");

ea0b716b8ce9694bb7d53597fdb9c65a0f0a4a43
freelw <826231693@qq.com>
2025-08-28 10:33:37 +0800

    update

--- ccall/cimport1/build.zig
+++ ccall/cimport1/build.zig
@@ -65,6 +65,9 @@ pub fn build(b: *std.Build) void {
         .root_module = exe_mod,
     });
 
+    exe.linkSystemLibrary("m");
+    exe.linkSystemLibrary("c");
+
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
     // step when running `zig build`).
